10107551bdf4d07b3f1416605c5aeee67adb8b21
Rhome Falaize <rhome.falaize@gmail.com>
2018-10-14 21:18:59 -0400

    Add piece images

--- deep-chess/src/components/chess.jsx
+++ deep-chess/src/components/chess.jsx
@@ -1,6 +1,18 @@
 import React, { Component } from "react";
 import { Game } from "../game";
 import "./chess.css";
+import wK from "./../assets/img/pieces/wK.png";
+import wN from "./../assets/img/pieces/wN.png";
+import wB from "./../assets/img/pieces/wB.png";
+import wP from "./../assets/img/pieces/wP.png";
+import wQ from "./../assets/img/pieces/wQ.png";
+import wR from "./../assets/img/pieces/wR.png";
+import bK from "./../assets/img/pieces/bK.png";
+import bN from "./../assets/img/pieces/bN.png";
+import bB from "./../assets/img/pieces/bB.png";
+import bP from "./../assets/img/pieces/bP.png";
+import bQ from "./../assets/img/pieces/bQ.png";
+import bR from "./../assets/img/pieces/bR.png";
 
 class ChessComponent extends Component {
   constructor() {
@@ -12,6 +24,25 @@ class ChessComponent extends Component {
 
   handleClick = square => {
     this.setState({ activeSquare: square });
+  };
+
+  getPieceImage = square => {
+    if (square.piece == null) return undefined;
+    if (square.piece.color === "W") {
+      if (square.piece.name === "P") return wP;
+      if (square.piece.name === "B") return wB;
+      if (square.piece.name === "N") return wN;
+      if (square.piece.name === "R") return wR;
+      if (square.piece.name === "Q") return wQ;
+      if (square.piece.name === "K") return wK;
+    } else {
+      if (square.piece.name === "P") return bP;
+      if (square.piece.name === "B") return bB;
+      if (square.piece.name === "N") return bN;
+      if (square.piece.name === "R") return bR;
+      if (square.piece.name === "Q") return bQ;
+      if (square.piece.name === "K") return bK;
+    }
   };
 
   renderBoard() {
@@ -26,7 +57,9 @@ class ChessComponent extends Component {
                 className={
                   this.state.activeSquare === square ? "active-square" : null
                 }
-              />
+              >
+                <img className="piece" src={this.getPieceImage(square)} />
+              </td>
             );
           })}
         </tr>
