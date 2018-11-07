a641f434ebf1e5070051f9c2daae3ee3a01c0deb
Rhome Falaize <rhome.falaize@gmail.com>
2018-11-07 07:51:22 -0500

    Add middleware

--- deep-chess-server/server.js
+++ deep-chess-server/server.js
@@ -11,6 +11,11 @@ app.use(bodyParser.json());
 var routes = require("./api/routes/agentRoutes"); //importing routes
 routes(app); //register the routes
 
+// add middleware to intercept unknown routes
+app.use(function(req, res) {
+  res.status(404).send({ url: req.originalUrl + " not found" });
+});
+
 app.listen(port);
 
 console.log("Agent RESTful API server started on: " + port);
