c6eaefd20338aaa14eae8c69cfeef36635a2d430
rfalaize <rhome.falaize@gmail.com>
2019-01-21 17:33:13 -0500

    Add cors

--- chess-engine/main.py
+++ chess-engine/main.py
@@ -2,29 +2,21 @@
 from flask import render_template
 import connexion
 import logging
+from flask_cors import CORS
 
 # Create a custom logger
 logging.getLogger().setLevel(logging.INFO)
 
 # Create handlers
 c_handler = logging.StreamHandler()
-# f_handler = logging.FileHandler('file.log')
 c_handler.setLevel(logging.INFO)
-# f_handler.setLevel(logging.ERROR)
-
-# Create formatters and add it to handlers
 c_format = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
-# f_format = logging.Formatter('%(asctime)s - %(levelname)s - %(name)s - %(message)s')
 c_handler.setFormatter(c_format)
-# f_handler.setFormatter(f_format)
-
-# Add handlers to the logger
 logging.getLogger().addHandler(c_handler)
-# logger.addHandler(f_handler)
 
 
 app = connexion.App(__name__, specification_dir='swagger/', options={"swagger_ui": True})
-# Read the swagger.yml file to configure the endpoints
+CORS(app.app)
 app.add_api('swagger.yml')
 
 # Routes
